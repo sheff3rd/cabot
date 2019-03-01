087ecd8e6fafc033402d465c3cf71dd04d4764f4
sheff3rd <thedoctorwhoovesmail@gmail.com>
2019-03-01 13:41:13 +0200

    initial commit

---
+++ columbus.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'columbus'
+  s.version     = '0.0.1'
+  s.date        = '2010-04-28'
+  s.summary     = 'FatoryBot alternative for trailblazer'
+  s.description = ''
+  s.authors     = ['Alexandre Ovcharov', 'Dmitriy Gladkiy', 'Vitaliy Stepura']
+  s.email       = 'sheff3rd@milestep.io'
+  s.files       = ['lib/columbus.rb']
+  s.homepage    = 'http://rubygems.org/gems/columbus'
+  s.license     = 'MIT'
+end
---
+++ lib/columbus.rb
@@ -0,0 +1,41 @@
+class Columbus
+
+  # GET
+  module Index
+    def self.call(model, current_user = {})
+      object = model.capitalize.constantize
+      params = Columbus::Parameters::Create.send(model)
+
+      object::Index.(params: params, current_user: current_user)
+    end
+  end
+
+  # GET /:id
+  module Show
+    def self.call(model, current_user = {})
+      object = model.to_s.capitalize.constantize
+      params = Columbus::Parameters::Show.send(model)
+
+      object::Show.(params: params, current_user: current_user)
+    end
+  end
+
+  # POST
+  module Create
+    def self.call(model, current_user = {})
+      object = model.to_s.capitalize.constantize
+      params = Columbus::Parameters::Create.send(model)
+
+      object::Create.(params: params, current_user: current_user)
+    end
+  end
+
+  # PUT
+  module Update
+    def self.call(model, current_user = {})
+      object = model.capitalize.constantize
+      params = Columbus::Parameters::Update.send(model)
+      object::Update.(params: params, current_user: current_user)
+    end
+  end
+end
